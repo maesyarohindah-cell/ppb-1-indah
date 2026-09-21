cd284ba27cd8fdfc04cff0acbf669479bce9218c
maesyarohindah-cell <maesyarohindah@gmail.com>
2026-09-21 12:07:46 +0700

    initial project

---
+++ sistem_bilangan.dart
@@ -0,0 +1,11 @@
+void main() {
+    int decimal = 10;
+    int octal = int.perse('12',radix);
+    int binary = int.perse('1010',radix; 2);
+    int hexadecimal =0xA;
+
+    print (decimal);
+    print (binary);
+    print (hexadecimal);
+    print (octal);
+}
